30bb2acbd2b1b0000ede071283c68847ab3bc074
Carlos-Larin <nekokar701@gmail.com>
2024-10-15 11:10:40 -0600

    hola

--- expo.py
+++ expo.py
@@ -1,39 +1,42 @@
 from PIL import Image, ImageChops, ImageEnhance, ImageOps, ImageDraw, ImageFont
 import os
 
-# Función del constructor
+#función del bob constructor
 def tilin():
-    # Verificar si la carpeta "imagenes" existe
+    """
+    #verificar si la carpeta "imagenes" existe 
     if not os.path.exists("imagenes"):
         os.makedirs("imagenes")
+    """
 
-    # Cargar la imagen
+    #cargar la imagen
     imagen = Image.open("imagenes/XD.jpg")
     
-    # Invertir colores
+    #invertir colores
     imageninvertida = ImageChops.invert(imagen)
     imageninvertida.save("imagenes/XD_colores.jpg")
     
-    # Resaltar luces (aumentar brillo)
+    #resaltar luces (aumentar brillo)
     enhancer = ImageEnhance.Brightness(imagen)
     imagen = enhancer.enhance(1.5)  # Aumentar brillo en un 50%
     imagen.save("imagenes/XD_luces.jpg")
 
-    # Crear un efecto espejo
+    #crear un efecto espejo
     cloveEspejo = ImageOps.mirror(imagen)
     cloveEspejo.save("imagenes/XD_espejo.jpg")
 
-    # Agregar texto a la imagen original
+    #agregar texto a la imagen original
     draw = ImageDraw.Draw(imagen)
     
-    # Cargar una fuente con tamaño grande (ej. Arial, 40)
+    #cargar una fuente con tamaño grande (ej. Arial, 40)
     try:
-        font = ImageFont.truetype("arial.ttf", 40)  # Cambia el tamaño aquí para hacerlo más grande o más pequeño
+        font = ImageFont.truetype("arial.ttf", 40)  #cambia el tamaño aquí para hacerlo más grande o más pequeño
     except IOError:
-        font = ImageFont.load_default()  # Usar la fuente predeterminada si no se encuentra la fuente "arial.ttf"
+        font = ImageFont.load_default()  #usar la fuente predeterminada si no se encuentra la fuente "arial.ttf"
     
-    draw.text((100, 100), "¡No pikees mid!", fill="white", font=font)  # Agregar texto con la fuente cargada
+    draw.text((100, 100), "¡No pikees mid!", fill="white", font=font)  #agregar texto con la fuente cargada
     imagen.save("imagenes/XD_letras.jpg")
 
+#aqui lanzamos el codigo inzano
 if __name__ == "__main__":
     tilin()
